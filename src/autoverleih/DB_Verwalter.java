bcdc081e702e21f0e3031891df5793fa5ee563c3
Raicandy <Raicandy@WIN-UASVQDSHDN1>
2015-11-12 19:47:54 +0100

    Erstellen der DB_Verwalter Daten.

--- src/autoverleih/DB_Verwalter.java
+++ src/autoverleih/DB_Verwalter.java
@@ -3,10 +3,19 @@
  */
 package autoverleih;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Steve
  */
 public class DB_Verwalter {
+    
+    List<Kunde> Kunden = new ArrayList<>();
+    List<Auto> Autos = new ArrayList<>();
+    List<Ausleihe> Ausleihen = new ArrayList<>();    
+    //List<Warteliste> Warteliste = new ArrayList<>();
+    //List<Rechnung> Rechnungen = new ArrayList<>();
     
 }
