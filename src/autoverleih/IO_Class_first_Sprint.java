c9b6d56ed7cca837ad3e09b6d36114338ffc346b
Fleescher <neubert.adrian@googlemail.com>
2015-11-19 18:47:42 +0100

    first checkin of Autoverleih tools

--- src/autoverleih/IO_Class_first_Sprint.java
+++ src/autoverleih/IO_Class_first_Sprint.java
@@ -13,6 +13,7 @@ import java.util.Locale;
 public class IO_Class_first_Sprint 
 //*************Erstellt von Adrian Neubert****************************#Fleescher        
 {
+    
     // Zum EInlesen der Tastatur-Eingabe wird ein Standart-Eingabe-Kanal verwendet.
     private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
     
@@ -60,6 +61,8 @@ public class IO_Class_first_Sprint
         return n;
     }
     
+     // Lesen eines date-Wertes von der Tastatur
+     // Exception falls kein gültiger date-Wert
     public static Date readdate() throws ParseException
     {
         String s = readString();
@@ -85,14 +88,89 @@ public class IO_Class_first_Sprint
         
         Formatierungen fürs Datum, gibt noch mehr, 
         sollten wir diskutieren welches Format!!!!
-        */
-                
-               
+        */      
     }
-    //**************************************************************************
-    /*
-    * Nur Tastatur-Eingabe!!
-    * Noch kein Dialog!!!! 
-    */
     
-}
+    public static void main(String[] args)
+    {
+        Auto auto = new Auto();
+        Kunde kunde = new Kunde();
+         System.out.println("Guten Tag");
+         System.out.println("Was möchten sie tun?");
+         System.out.println("(K)unden bearbeiten, (A)utos bearbeiten");
+         //Bearbeitungsdialog für Kunden
+         if(readString() == "K" )
+         {
+             System.out.println("-----Kunden-----");
+             System.out.println("Kunde (E)rstellen --- Kunde (L)öschen");
+             if(readString() == "E")
+             {
+                 System.out.println("---Neuer Kunde---");// Daten des neuen Kunden eingeben
+                 System.out.println("Kunden ID: ");
+                 kunde.setKunden_ID(readint());          
+                 System.out.println("Nachname: ");
+                 kunde.setNachname(readString());
+                 System.out.println("Vorname: ");
+                 kunde.setVorname(readString());
+                 //gibt noch mehr Kundendaten war zu faul die noch einzutragen :D
+                 //Für einige Dateneingaben müssen oben noch Methoden entwickelt werden
+             }
+             else
+             {
+                 System.out.println("Bitte geben sie nur E oder L ein!");
+             }
+             if(readString() == "L")// Kundendaten löschen
+             {
+                 System.out.println("---Kunde löschen ---");
+                 kunde.setKunden_ID(0);          
+                 kunde.setNachname(null);
+                 kunde.setVorname(null);
+             }
+             else
+             {
+                 System.out.println("Bitte geben sie nur E oder L ein!");
+             }
+         }
+         else
+         {
+             System.out.println("Bitte geben sie nur K oder A ein!");
+         }
+         
+         // Bearbeitungsdialog für Autos
+          if(readString() == "A" )
+         {
+             System.out.println("-----Auto-----");
+             System.out.println("Auto (E)rstellen --- Auto (L)öschen");
+             if(readString() == "E")
+             {
+                 System.out.println("---Neues Auto---");// Autodaten eingeben
+                 System.out.println("Auto ID: ");
+                 auto.setAuto_ID(readint());
+                 System.out.println("Hersteller: ");
+                 auto.setHersteller(readString());
+                 System.out.println("Kaution: ");
+                 auto.setKaution(readdouble());
+                 // gibt noch mehr Autodaten war zu faul alle hier hin zu schreiben :D
+                 // Für einige Daten müssen oben noch Methoden entwickelt werden
+             }
+             else
+             {
+                 System.out.println("Bitte geben sie nur E oder L ein!");
+             }
+             
+             if(readString() == "L")// Auto löschen
+             {
+                 System.out.println("---Auto löschen ---");
+                 auto.setAuto_ID(0);
+                 auto.setHersteller(null);
+                 auto.setKaution(0);
+             }
+         }
+         else
+         {
+             System.out.println("Bitte geben sie nur K oder A ein!");
+         }
+         
+         System.out.println("");
+    }
+} //***************************Ende Adrian Neubert #Fleescher*******************
