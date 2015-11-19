cdd38b9508388f3e083721369c891e4d938b312c
Fleescher <neubert.adrian@googlemail.com>
2015-11-19 11:27:59 +0100

    first checkin of Autoverleih tools

--- src/autoverleih/IO_Class_first_Sprint.java
+++ src/autoverleih/IO_Class_first_Sprint.java
@@ -1,15 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package autoverleih;
 
+import java.io.*;
 /**
  *
  * @author Raicandy(Punker)
  */
-public class IO_Class_first_Sprint {
-    //dialog system reicht
+public class IO_Class_first_Sprint 
+//*************Erstellt von Adrian Neubert****************************#Fleescher        
+{
+    // Zum EInlesen der Tastatur-Eingabe wird ein Standart-Eingabe-Kanal verwendet.
+    private static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+    
+    /**
+     * Lesen einer Zeichenkette von der Tastatur. 
+     * Theoretisch mögliche IOException wird ohne Reaktion abgefangen
+    **/
+    public static String readString()
+    {
+        String s = "";
+        try
+        {
+            s = input.readLine();
+        }
+        catch(IOException e)
+        {
+            // keine Reaktion
+        }
+        
+        return s;
+    }
+     // Lesen eines int-Wertes von der Tastatur
+     // Exception falls kein gültiger int-Wert
+    public static int readint() throws NumberFormatException
+    {
+        String s = readString();
+        int n = Integer.parseInt(s);
+        return n;
+    }
+     // Lesen eines float-Wertes von der Tastatur
+     // Exception falls kein gültiger float-Wert
+    public static float readfloat() throws NumberFormatException
+    {
+        String s = readString();
+        float n = Float.valueOf(s).floatValue();
+        return n;
+    }
+    
+    // Lesen eines double-Wertes von der Tastatur
+    // Exception falls kein gültiger double-Wert
+    public static double readdouble()throws NumberFormatException
+    {
+        String s = readString();
+        double n = Double.valueOf(s).doubleValue();
+        return n;
+    }
+    //**************************************************************************
+    /*
+    * Nur Tastatur-Eingabe!!
+    * Noch kein Dialog!!!! 
+    */
     
 }
