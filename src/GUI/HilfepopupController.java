9ddef5c425f33e41e6497b886bdb64155c52145e
DMeer <DMeer@NSA>
2016-01-18 18:59:54 +0100

    About Popup mit Inhalt gefüllt.

--- src/GUI/HilfepopupController.java
+++ src/GUI/HilfepopupController.java
@@ -13,6 +13,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -33,7 +34,20 @@ public class HilfepopupController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-	// TODO
+        
+        imgView.setImage(new Image ("Data/HSMW_Logo.png"));
+        
+        nameLabel.setText("Autoverleih: Finale Version, 2. Sprint \n\n "+
+                    "Projektteam: \n"+
+                        "   Projektleiter: Daniel Meerwald \n"+
+                        "   Softwarearchitekt: Christopher Haack \n"+
+                        "   Chefprogrammierer: Denis Brousillon \n"+
+                        "   Dokumentenbeauftragter: Steve Vogel \n"+
+                        "   Testmanager: Adrian Neubert \n"+
+                        "   Tester: Danilo May \n"+
+                        "   Projektmanagerin: Ribanna Meister \n");
+        
+	
     }    
 
     @FXML
